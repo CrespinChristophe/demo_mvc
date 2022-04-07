fefdab8553cd7c0b1e2b825d41f28e31e7275529
Crespin <crespin.christophe@netc.eu>
2022-04-07 15:49:58 +0200

    dernier commit et ne fct pas

--- demo_mvc/src/main/java/technifutur/crespin/demo_mvc/controllers/ProduitController.java
+++ demo_mvc/src/main/java/technifutur/crespin/demo_mvc/controllers/ProduitController.java
@@ -5,12 +5,15 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import technifutur.crespin.demo_mvc.models.Produit;
 import technifutur.crespin.demo_mvc.service.ProduitService;
 
+import java.util.List;
 import java.util.NoSuchElementException;
 
 @Controller//ce n'est pas un controller de REST API donc simplement controller
+@RequestMapping("/produit")
 public class ProduitController {
 
     private final ProduitService service;
@@ -19,7 +22,7 @@ public class ProduitController {
         this.service = service;
     }
 
-    @GetMapping("/produit/{id}")
+    @GetMapping("/{id}")
     public String displayOne(@PathVariable int id, Model model){
 
         Produit p = service.getOne(id);
@@ -27,6 +30,13 @@ public class ProduitController {
 
 
         return "displayOne";
+    }
+
+    @GetMapping
+    public String displayAll(Model model){
+        List<Produit> list = service.getAll();
+        model.addAttribute("liste_produit", list);
+        return "displayAll";
     }
 
     @ExceptionHandler(NoSuchElementException.class)
